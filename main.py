5a2f675f704d5b0a0952f15549f06c9dff5d097d
nimag2000 <nimag2000@gmail.com>
2025-02-25 02:11:09 -0600

    Update main.py

--- main.py
+++ main.py
@@ -23,4 +23,5 @@ def market_data():
     return jsonify(data)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use the PORT provided by Render or default to 5000
+    app.run(host="0.0.0.0", port=port, debug=True)
